7bec4f9d75bf5da562bb58dd635819213da45ff6
plphuc <phamluumyphuc@gmail.com>
2024-08-15 23:41:35 +0700

    routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import HomePage from './pages/HomePage'
+import SignUpPage from './pages/SignUpPage'
+import LoginPage from './pages/LoginPage'
+import ProfilePage from './pages/ProfilePage'
+import NotFoundPage from './pages/NotFoundPage'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="text-white">
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/sign-up" element={<SignUpPage />} />
+                <Route path="/log-in" element={<LoginPage />} />
+                <Route path="/profile/:userId" element={<ProfilePage />} />
+                <Route path="*" element={<NotFoundPage />} />
+            </Routes>
+        </div>
+    )
 }
 
-export default App;
+export default App
